aa5a0324e749d7ad51664c498b27a10685edd311
Kaio Silveira <silveira.kaio@icloud.com>
2024-03-29 13:39:06 +0000

    assigning-to-input-param: split `inputValue` by introducing `originalInputValue`

--- src/assigning-to-input-param/index.js
+++ src/assigning-to-input-param/index.js
@@ -1,4 +1,5 @@
-export function discount(inputValue, quantity) {
+export function discount(originalInputValue, quantity) {
+  let inputValue = originalInputValue;
   if (inputValue > 50) inputValue = inputValue - 2;
   if (quantity > 100) inputValue = inputValue - 1;
   return inputValue;
